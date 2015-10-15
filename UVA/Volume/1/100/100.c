5482223091c99e2563a472061d49688fe2692d4d
wtianzhi <wtianzhi@gmail.com>
2015-10-14 20:22:27 -0600

    Started UVA directory

---
+++ UVA/Volume/1/100/100.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+int threenplusone (int n) {
+
+    // Base case
+    if (n == 1) {
+	 return n;
+    }
+
+    //
+
+}
+
+
+int main(void) 
+{
+    int a;
+
+    return 0;
+
+}
